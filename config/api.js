b959f583dbceff82888a4d75975a9599a6312942
Gengxin Li <ligengxin96@gmail.com>
2021-06-16 10:13:20 +0800

    add test

---
+++ config/api.js
@@ -0,0 +1,5 @@
+export default [
+  'https://getbingimages.herokuapp.com/v1/getImages',
+  'https://getbingimages.herokuapp.com/v1/getImages/en-us',
+  'https://getbingimages.herokuapp.com/v1/getImages/random',
+]
---
+++ test/apiHealthCheck.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import assert from 'assert';
+import dotenv from 'dotenv';
+
+import apis from '../config/api.js';
+import getImageRequest from '../utils/getImageRequest.js';
+
+dotenv.config();
+
+describe('test server health', function() {
+  describe('test mongoose server health', function() {
+    it('should connect mongoose db successful', async function() {
+      this.retries(2);
+      try {
+        const response = await mongoose.connect(process.env.CONNECTION_URL);
+        assert.notStrictEqual(response, undefined);
+        assert.notStrictEqual(response, null);
+      } catch (error) {
+        assert(false);
+      }
+    });
+  });
+  
+  describe('test api health', function() {
+    it('api should return image info', async function() {
+      this.retries(2);
+      try {
+        for await (const api of apis) {
+          const result = await getImageRequest(api);
+          const imgs = JSON.parse(result).data;
+          assert.notStrictEqual(imgs, undefined);
+          assert.notStrictEqual(imgs, null);
+        }
+      } catch (error) {
+        assert(false);
+      }
+    });
+  });
+})
