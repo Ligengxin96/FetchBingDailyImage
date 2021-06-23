458824dccdd018667d050062cc7164d389677ff5
Ligengxin <Ligengxin96@gmail.com>
2021-06-23 23:10:27 +0800

    fix bodyParser deprecation

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ const port = process.env.PORT || 5000;
 const databaseConnectStr = process.env.CONNECTION_URL;
 
 const app = express();
-app.use('/v1/getimages', imageRouter);
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+
+app.use('/v1/getimages', imageRouter);
 
 mongoose.connect(databaseConnectStr, { 
   useNewUrlParser: true, 
