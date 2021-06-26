57eea8cd317215304bff0c557fe7a448b8856cc7
Ligengxin <Ligengxin96@gmail.com>
2021-06-26 20:51:47 +0800

    add new api

--- config/getBingImagesServerApi.js
+++ config/getBingImagesServerApi.js
@@ -1,5 +1,6 @@
 export default [
   'https://getbingimages.herokuapp.com/v1/getimages',
-  'https://getbingimages.herokuapp.com/v1/getimages/en-us',
+  'https://getbingimages.herokuapp.com/v1/getimages/zh-cn',
   'https://getbingimages.herokuapp.com/v1/getimages/random',
+  'https://getbingimages.herokuapp.com/v1/getimages/random/en-us',
 ]
