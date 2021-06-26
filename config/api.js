8f8f6a6124714a07e69d4ad4ae20705f50d85d72
Ligengxin <Ligengxin96@gmail.com>
2021-06-26 20:52:47 +0800

    delete new api

--- config/api.js
+++ config/api.js
@@ -2,5 +2,4 @@ export default [
   'https://getbingimages.herokuapp.com/v1/getimages',
   'https://getbingimages.herokuapp.com/v1/getimages/en-us',
   'https://getbingimages.herokuapp.com/v1/getimages/random',
-  'https://getbingimages.herokuapp.com/v1/getimages/random/zh-cn',
 ]
