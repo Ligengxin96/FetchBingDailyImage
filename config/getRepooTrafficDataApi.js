157e2911e84a6ef6d3b0bbd9f027fc3c4962d1e7
Ligengxin <Ligengxin96@gmail.com>
2021-06-29 23:51:37 +0800

    add more api

--- config/getRepooTrafficDataApi.js
+++ config/getRepooTrafficDataApi.js
@@ -1,3 +1,4 @@
 export default [
-  'http://getrepootrafficdata.herokuapp.com/v1/getrepoinfo/FetchRepooTrafficData/1'
+  'http://getrepootrafficdata.herokuapp.com/v1/getrepoinfo/FetchRepooTrafficData/1',
+  'http://getrepootrafficdata.herokuapp.com/v1/getrepoinfo/FetchRepooTrafficData/1?aggregate=true&sort=asc'
 ]
