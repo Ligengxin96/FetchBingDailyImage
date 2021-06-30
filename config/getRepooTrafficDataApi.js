63f7ddfb5d7a1d38ee90d0e137b63e8c69fac9ee
Ligengxin <Ligengxin96@gmail.com>
2021-06-30 17:44:05 +0800

    fix

--- config/getRepooTrafficDataApi.js
+++ config/getRepooTrafficDataApi.js
@@ -1,4 +1,4 @@
 export default [
-  'http://getrepootrafficdata.herokuapp.com/v1/getrepoinfo/FetchRepooTrafficData/1',
-  'http://getrepootrafficdata.herokuapp.com/v1/getrepoinfo/FetchRepooTrafficData/1?aggregate=true&sort=asc'
+  'http://getrepootrafficdata.herokuapp.com/v1/getrepoinfo/FetchRepoTrafficData/1',
+  'http://getrepootrafficdata.herokuapp.com/v1/getrepoinfo/FetchRepoTrafficData/1?aggregate=true&sort=asc'
 ]
