da7e2771d25c4ebb235cd086f0474a732764e58c
Ligengxin <Ligengxin96@gmail.com>
2023-06-18 00:09:27 -0400

    update

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker stop fetchBingDailyImage && docker rm fetchBingDailyImage
-docker run -v /etc/localtime:/etc/localtime --name fetchBingDailyImage --network userDefined --ip 172.20.0.6 -d fetch-bing-daily-image
+docker run -v /etc/localtime:/etc/localtime --name fetchBingDailyImage --network userDefined -d fetch-bing-daily-image
